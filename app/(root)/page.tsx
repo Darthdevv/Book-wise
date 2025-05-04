248cbb98ddc2e0329ebd5da79d94388190c572a4
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2025-05-04 04:24:01 +0300

    � feat: Header added

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+
+
+
+const Home = () => (
+    <>
+      <BookOverview />
+      <BookList />
+    </>
+  );
+
+export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
 import animate from "tailwindcss-animate";
 
-module.exports = {
-  darkMode: "class",
+export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,22 +15,23 @@ module.exports = {
         "bebas-neue": ["var(--bebas-neue)"],
       },
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "#E7C9A5",
-          admin: "#25388C",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        // primary: {
+        //   DEFAULT: "hsl(var(--primary))",
+        //   foreground: "hsl(var(--primary-foreground))",
+        // },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
         },
         muted: {
           DEFAULT: "hsl(var(--muted))",
@@ -40,13 +41,23 @@ module.exports = {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))",
         },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+        primary: {
+          DEFAULT: "#E7C9A5",
+          admin: "#25388C",
         },
         green: {
           DEFAULT: "#027A48",
@@ -88,6 +99,9 @@ module.exports = {
           100: "#CBD5E1",
         },
       },
+      screens: {
+        xs: "480px",
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
@@ -99,4 +113,4 @@ module.exports = {
     },
   },
   plugins: [animate],
-};
+} satisfies Config;
